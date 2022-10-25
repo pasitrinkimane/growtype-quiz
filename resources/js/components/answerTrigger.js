baf1e08d190f2497dee9ea935209fd6f1d59c58b
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-10-25 15:05:38 +0300

    fixes

--- resources/js/components/answerTrigger.js
+++ resources/js/components/answerTrigger.js
@@ -1,6 +1,5 @@
 export function answerTrigger() {
     $('.b-quiz-question-answers .b-quiz-question-answer').click(function () {
-
         if ($(this).attr('data-url').length > 0) {
             window.location = $(this).attr('data-url');
         }
@@ -15,6 +14,19 @@ export function answerTrigger() {
             if ($(this).closest('.b-quiz-question').attr('data-answer-type') === 'multiple') {
                 $(this).removeClass('is-active');
             }
+        }
+    });
+
+    /**
+     * Hover
+     */
+    $('.b-quiz-question-answers .b-quiz-question-answer').hover(function () {
+        var imgUrl = $(this).attr('data-img-url');
+
+        if (imgUrl.length > 0) {
+            $(this).closest('.b-quiz-question').find('.b-img .e-img').css({
+                "background-image": "url( " + imgUrl + " )"
+            });
         }
     });
 }
